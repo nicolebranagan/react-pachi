d70bc023350738c2887849394c0f3b308cdfd484
Nicole <nicolecarolinebranagan@gmail.com>
2023-04-14 13:15:18 -0400

    Place a debug timer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
+import * as React from "react";
 import { usePachinkoState } from "../hooks/PachinkoDataLines";
 
 function App() {
   const pachinkoState = usePachinkoState();
+  const { dbgSendButton, state, ...rest } = pachinkoState;
 
-  return <div className="App">{JSON.stringify(pachinkoState)}</div>;
+  return (
+    <div className="App">
+      {JSON.stringify(rest)}
+      <div>
+        {state.map((s, i) => (
+          <div key={i}>{JSON.stringify(s)}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/PachinkoDataLines.js
+++ src/hooks/PachinkoDataLines.js
@@ -80,6 +80,7 @@ export const usePachinkoState = () => {
 
   return {
     dbgSendButton,
+    state,
     ...results,
   };
 };
