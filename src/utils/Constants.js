735a6f61b489b429aa5207f48882cf0ab1e21523
Nicole <nicolecarolinebranagan@gmail.com>
2023-04-14 20:04:20 -0400

    Save previous states to local storage

---
+++ src/utils/Constants.js
@@ -0,0 +1,5 @@
+// These must correspond to the buttons on the gamepad
+export const RESET_BUTTON = 0;
+export const JACKPOT_BUTTON = 1;
+export const BALLS_WON_BUTTON = 2;
+export const START_BUTTON = 3;
