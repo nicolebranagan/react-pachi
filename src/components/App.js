5e19ce371aefbe6c78773013af479fdd93658c76
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2023-04-16 13:59:53 -0400

    Create border

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,29 @@ import { usePachinkoState } from "../hooks/PachinkoDataLines";
 
 function App() {
   const pachinkoState = usePachinkoState();
-  const { dbgSendButton, state, dbgButtonStatus, ...rest } = pachinkoState;
+  const { dbgSendButton, state, dbgButtonStatus, inJackpot, ...rest } =
+    pachinkoState;
+
+  if (window.location.hash === "#debug")
+    return (
+      <div className="App">
+        {JSON.stringify(rest)}
+        <div>{JSON.stringify(dbgButtonStatus)}</div>
+        <div>
+          {state.map((s, i) => (
+            <div key={i}>{JSON.stringify(s)}</div>
+          ))}
+        </div>
+      </div>
+    );
+
+  const jackpotClass = inJackpot ? "jackpot" : "no-jackpot";
 
   return (
-    <div className="App">
-      {JSON.stringify(rest)}
-      <div>
-        {JSON.stringify(dbgButtonStatus)}
-      </div>
-      <div>
-        {state.map((s, i) => (
-          <div key={i}>{JSON.stringify(s)}</div>
-        ))}
-      </div>
+    <div className={`wrapper ${jackpotClass}`}>
+      <div className="border border-top" />
+      <div className={`main-body ${jackpotClass}`}></div>
+      <div className="border border-bottom" />
     </div>
   );
 }
